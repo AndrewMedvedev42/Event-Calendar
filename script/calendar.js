64ecc730e90ea62867413341fed2a25257f8b668
AndrewMedvedev42 <sanjino56@gmail.com>
2021-02-07 20:05:25 +0200

    Style fix

--- script/calendar.js
+++ script/calendar.js
@@ -1,11 +1,3 @@
-//-------------------------
-//-------------------------
-//-------------------------
-// function ID(){
-//     let randonId = `${Math.random().toString(36).substr(2, 9)}`
-//     return randonId ;
-// };
-
 function setValueSlot(){
     chooseDom.style.backgroundColor = "lightgreen"
     chooseDom.style.display = "flex"
@@ -20,7 +12,6 @@ function setImg(slotId, slotElement){
     chooseDom.appendChild(imageX)
     imageX.onclick = function(){
         localStorage.removeItem(slotId)
-        // location.reload()
         slotElement.innerHTML = "_"
         slotElement.style.backgroundColor = "white"
         slotElement.style.justifyContent = "center"
@@ -39,64 +30,3 @@ if(localStorage.length === 0){
     
     setImg(idName2, chooseDom)
 }}
-
-// function deleteBtn(){
-//     for(let cont = 0; cont < localStorage.length; cont++){
-//         var pen = document.getElementsByClassName("deletebtn")
-//         var pens = pen[cont]
-//         if(pens.parentNode.id == localStorage.key(cont)){
-//             console.log("Gotcha!!!")
-//         }else{
-//             console.log("nope")
-//         }
-//     }
-// }
-
-var pen = document.getElementsByClassName("deletebtn")
-
-
-function findImgData(){
-    for(let key in pen){
-        if(pen[key].parentNode.id != findStoreData()){
-            console.log("poop")
-        }
-    }
-}
-
-
-function findStoreData(){
-    for(let key in localStorage){
-
-    }
-}
-
-
-
-
-
-
-//  pens.addEventListener("click", XDelete)
-
-
-// const pen = document.getElementsByClassName("deletebtn")
-// let pens = pen[4]
-// console.log(pens.parentNode.id)
-
-
-// const XBtn = document.getElementsByClassName(".deletebtn")
-
-// function deleteBtn(){
-        
-//         console.log(closeBtn)
-//         //  const parentClassName = closeBtn.parentNode.className
-//         //  console.log(parentClassName)
-
-//         // localStorage.removeItem(closeBtnParentId)
-//         // let idName2 = localStorage.key(counter)
-//         // let chooseDom = document.getElementById(`${idName2}`)
-
-//         // chooseDom.innerHTML = "_"
-//         // chooseDom.style.backgroundColor = "white"
-//         // chooseDom.style.justifyContent = "center"
-// }
-// XBtn.addEventListener("click", deleteBtn);
